6b2b579d8e890f5c9592094004c28579884952ad
isha <isha@fedora>
2023-12-07 11:26:25 +0100

    Ajout de division

---
+++ src/Atelier8/Question1.java
@@ -0,0 +1,13 @@
+package Atelier8;
+
+public class Question1 {
+	public class division {
+		public static void main(String[] args) {
+			int i = 1000, j;
+			do {
+				i--;
+				j = 1 / i;
+			} while (true);
+		}
+	}
+}
