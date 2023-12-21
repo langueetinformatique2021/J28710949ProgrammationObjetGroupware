b558cbffbade1600809e8fe860f3c9e6808772d3
isha <isha@fedora>
2023-12-21 10:21:32 +0100

    Exercice 2 1)

---
+++ src/Atelier9/Copie.java
@@ -0,0 +1,12 @@
+package Atelier9;
+
+import java.io.File;
+
+public class Copie {
+	public static void main(String[] args) {
+		File fent = new File("Noms.txt");
+		System.out.println(fent.length());
+		
+
+	}
+}
