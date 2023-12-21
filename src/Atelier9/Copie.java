e75e9e0887f9683038ff108ea7386476bd769518
isha <isha@fedora>
2023-12-21 10:33:31 +0100

    Copie (ex2)

--- src/Atelier9/Copie.java
+++ src/Atelier9/Copie.java
@@ -1,12 +1,25 @@
 package Atelier9;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.FileOutputStream;
 
 public class Copie {
+	
 	public static void main(String[] args) {
-		File fent = new File("Noms.txt");
+		File fent = new File("Orphila.txt");
 		System.out.println(fent.length());
+		try {
+		FileInputStream f1 = new FileInputStream(fent);
+		System.out.println(f1.available());
 		
-
+		FileOutputStream f2 = new FileOutputStream (new File ("Orphilacopie.txt"));
+		
+		f1.close();
+		f2.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
